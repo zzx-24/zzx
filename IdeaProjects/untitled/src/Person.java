7804c7d27320dd278ffcb6e9f3772535a8e670c0
客人用户 <Guest@tongchongdeMacBook-Air.local>
2019-12-04 15:44:55 +0800

    first

---
+++ IdeaProjects/untitled/src/Person.java
@@ -0,0 +1,8 @@
+public class Person {
+    Student student = new Student();
+    public Person(){
+        System.out.println("person");
+
+
+    }
+}
---
+++ IdeaProjects/untitled/src/TestBook.java
@@ -0,0 +1,7 @@
+public class TestBook {
+    public static void main (String[] args){
+        Book b = new Book("java基础", 233);
+        System.out.println(b);
+    }
+
+}
